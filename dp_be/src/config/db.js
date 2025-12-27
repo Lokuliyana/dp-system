55805de3a520386e5316d200d36bdad110c34dab
Chandupa Lokuliyana <lokuliyanacp@gmail.com>
2025-12-28 01:32:03 +0530

    feature: api response

--- dp_be/src/config/db.js
+++ dp_be/src/config/db.js
@@ -1,15 +1,41 @@
 const mongoose = require('mongoose')
 const env = require('./env')
 
-module.exports = async function connectDB() {
-  mongoose.set('strictQuery', true)
-  
-  const maskedUri = env.mongoUri.replace(/:([^:@]+)@/, ':****@')
-  console.log(`Connecting to MongoDB: ${maskedUri}`)
+let cached = global.mongoose
 
-  await mongoose.connect(env.mongoUri, {
-    dbName: env.mongoDbName,
-    maxPoolSize: env.mongoPoolSize
-  })
-  console.log('MongoDB connected')
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+module.exports = async function connectDB() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      dbName: env.mongoDbName,
+      maxPoolSize: env.mongoPoolSize,
+      bufferCommands: false, // Disable mongoose buffering
+    }
+
+    mongoose.set('strictQuery', true)
+    
+    const maskedUri = env.mongoUri.replace(/:([^:@]+)@/, ':****@')
+    console.log(`Connecting to MongoDB: ${maskedUri}`)
+
+    cached.promise = mongoose.connect(env.mongoUri, opts).then((mongoose) => {
+      console.log('MongoDB connected')
+      return mongoose
+    })
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+
+  return cached.conn
 }
